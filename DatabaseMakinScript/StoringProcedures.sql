3e5c28c67700540ec5340621f94ca0b28d3f8739
redmouce <redmouce@ya.ru>
2016-04-01 09:36:49 +0300

    fixed table making script add some functions with return# Please enter the commit message for your changes. Lines starting

--- DatabaseMakinScript/StoringProcedures.sql
+++ DatabaseMakinScript/StoringProcedures.sql
@@ -28,7 +28,7 @@ BEGIN
 	SELECT Project.ID as 'ProjectID', Project.Name as 'ProjectName' FROM Project
 		INNER JOIN ProjectIssueMap ON ProjectIssueMap.ProjectID = Project.ID
 			INNER JOIN Issue ON ProjectIssueMap.IssueID = Issue.ID
-				INNER JOIN Employee ON Issue.EmployeeID = Employee.ID AND Employee.ID = EmployeeID
+				INNER JOIN Employee ON Issue.EmployeeID = Employee.ID AND Employee.ID = @EmployeeID
 END
 
 GO
@@ -45,7 +45,7 @@ BEGIN
 		INNER JOIN ProjectIssueMap ON ProjectIssueMap.ProjectID = Project.ID
 			INNER JOIN Issue ON ProjectIssueMap.IssueID = Issue.ID
 				INNER JOIN Employee ON Issue.EmployeeID = Employee.ID AND Employee.ID = @EmployeeID
-	WHERE ProjectID = Project.ID
+	WHERE @ProjectID = Project.ID
 END
 
 GO
@@ -58,12 +58,36 @@ CREATE PROCEDURE GetEmployeeIssues
 AS
 BEGIN
 	SELECT Employee.ID, Employee.Name, Issue.Name as 'IssueName' FROM Issue
-		INNER JOIN Employee ON Issue.EmployeeID = Employee.ID AND Employee.ID = EmployeeID
+		INNER JOIN Employee ON Issue.EmployeeID = Employee.ID AND Employee.ID = @EmployeeID
 END
 
 GO
 
 --Getting and returning parameters
 
+IF OBJECT_ID ( 'GetGroopAccess', 'P' ) IS NOT NULL 
+    DROP PROCEDURE GetGroopAccess;
+GO
+CREATE PROCEDURE GetGroopAccess
+	@GroopID uniqueidentifier
+AS
+BEGIN
+	RETURN (SELECT AccessLevel.[View], AccessLevel.Comment, AccessLevel.MakeIssue FROM AccessLevel
+		INNER JOIN Groop ON AccessLevel.ID = Groop.ID )
+END
 
+GO
 
+IF OBJECT_ID ( 'GetProjectEmployeeAccess', 'P' ) IS NOT NULL 
+    DROP PROCEDURE GetProjectEmployeeAccess;
+GO
+CREATE PROCEDURE GetProjectEmployeeAccess
+	@EmployeeID uniqueidentifier,
+	@ProjectID uniqueidentifier
+AS
+BEGIN
+	RETURN (SELECT AccessLevel.[View], AccessLevel.Comment, AccessLevel.MakeIssue FROM AccessLevel
+		INNER JOIN ProjectGroopMap ON ProjectGroopMap.AccessID = AccessLevel.ID
+			INNER JOIN Groop ON ProjectGroopMap.ProjectID = @ProjectID AND Groop.ID = ProjectGroopMap.GroopID
+				INNER JOIN GroopEmployeeMap ON Groop.ID = GroopEmployeeMap.GroopID AND GroopEmployeeMap.EmployeeID = @EmployeeID )
+END
